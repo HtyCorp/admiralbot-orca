1087ada9461d2b31b3fb7156ecf7a04ab7469dcd
Hamish Tym <hamishtym@bigpond.com>
2023-08-03 20:50:08 +1000

    Disable handler warmup request

--- src/main/java/com/admiralbot/orca/handler/lambda/InteractionPostLambdaHandler.java
+++ src/main/java/com/admiralbot/orca/handler/lambda/InteractionPostLambdaHandler.java
@@ -8,7 +8,13 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import lombok.extern.log4j.Log4j2;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Log4j2
@@ -23,8 +29,34 @@ public class InteractionPostLambdaHandler extends DelegateHandler<APIGatewayV2HT
         var interactionAuthenticator = new InteractionAuthenticator(true, discordAppKeySet);
         var commandDispatcher = new CommandDispatcher();
 
-        // TODO: Perform class pre-loading here
+        preloadClasses();
 
         return new InteractionPostHandler(interactionAuthenticator, commandDispatcher);
+    }
+
+    private void preloadClasses() {
+
+        var classNamesUrl = Objects.requireNonNull(getClass().getClassLoader().getResource("preloadClassNames.txt"));
+
+        // Disable verbose class loading for the duration of this method, since we want to track "[class,load]" messages outside preload context to see if we
+        // missed any classes in the list.
+        var classLoaderSettings = ManagementFactory.getClassLoadingMXBean();
+        var previousVerboseState = classLoaderSettings.isVerbose();
+        classLoaderSettings.setVerbose(false);
+
+        try (var lines = Files.lines(Paths.get(classNamesUrl.toURI()))) {
+            lines.forEach(name -> {
+                try {
+                    Class.forName(name);
+                } catch (Exception e) {
+                    // This has the potential to create a lot of log pollution, but it would only be in version-publish streams, so it's limited.
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Failed to read preload class names list", e);
+        } finally {
+            classLoaderSettings.setVerbose(previousVerboseState);
+        }
     }
 }
